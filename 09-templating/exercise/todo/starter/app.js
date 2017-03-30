d8fd3b7333f1007f6260f8bc0e8f392cd7b6d162
Maryam1362 <maryam.sarab@gmail.com>
2017-03-30 18:38:04 -0400

    all

--- 09-templating/exercise/todo/starter/app.js
+++ 09-templating/exercise/todo/starter/app.js
@@ -1,14 +1,35 @@
 var data = {
   todos: [
-    'learn html',
-    'learn css',
-    'learn js'
+    'Learn HTML',
+    'Learn CSS',
+    'Learn jQuery'
   ]
+  
 }
 
-var app = document.getElementById('app')
-var templateSource = document.getElementById('to-do-template')
-var template = Handlebars.compile( templateSource.innerHTML )
-var renderedTemplate = template( data )
+function setup() {
+  renderView()
 
-app.innerHTML = renderedTemplate
+  document.getElementById('form').addEventListener('submit', addTodo)
+}
+
+function renderView() {
+  var app = document.getElementById('app')
+  var templateSource = document.getElementById('to-do-template')
+  var template = Handlebars.compile( templateSource.innerHTML )
+  var renderedTemplate = template( data )
+  app.innerHTML = renderedTemplate
+}
+
+function addTodo( e ) {
+  e.preventDefault()
+  var todoInput = e.target.querySelector('input')
+
+  var newTodo = todoInput.value
+  data.todos.push( newTodo )
+  todoInput.value = ''
+
+  renderView()
+}
+
+window.onload = setup
